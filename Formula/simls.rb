d9a018534c74031af850013bc9aaf428e087ed25
Jeroen <46943193+jeroenzeelmaekers@users.noreply.github.com>
2024-04-12 18:17:15 +0200

    feat(simls): update to v0.0.2

--- Formula/simls.rb
+++ Formula/simls.rb
@@ -1,14 +1,14 @@
 class Simls < Formula
-  version "v0.0.1"
+  version "v0.0.2"
   homepage "https://github.com/benja/cloup"
   desc "Simls is a cli tool that allows for managing of android emulators & iOS simulators."
   
   if OS.mac?
     url "https://github.com/jeroenzeelmaekers/simls/releases/download/#{version}/simls-x86_64-apple-darwin.tar.gz"
-    sha256 "4dcb69923d0c9e590424d7645019c39cad47e60d672e11848bcd899f9a7c7ee7"
+    sha256 "8f7ee6b51defd977ae235b6aeeae487ce2582175cdcf22229497ba811642b31f"
   elsif OS.windows?
     url "https://github.com/jeroenzeelmaekers/simls/releases/download/#{version}/simls-x86_64-pc-windows-msvc.zip"
-    sha256 "fd839a793340a7d72e392e70739b81c723dca27cfac235b21009a54033de21c5"
+    sha256 "04ad00966a6a4ed99381958a3c65599311abf9f3258cdbd47af93dbdc91e298e"
   else
     odie "Unsupported operating system"
   end
