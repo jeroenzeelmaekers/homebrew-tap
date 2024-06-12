9726c80185461e3e873c35c6d07c54ed94c9636e
Jeroen Zeelmaekers <mail@jeroenzeelmaekers.com>
2024-06-12 21:20:51 +0200

    feat(simls): v0.0.3

--- Formula/simls.rb
+++ Formula/simls.rb
@@ -1,24 +1,24 @@
 class Simls < Formula
-  version "v0.0.2"
+  version "v0.0.3"
   homepage "https://github.com/jeroenzeelmaekers/simls"
   desc "Simls is a cli tool that allows for managing of android emulators & iOS simulators."
-  
-  if OS.mac?
+
+  if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/jeroenzeelmaekers/simls/releases/download/#{version}/simls-x86_64-apple-darwin.tar.gz"
-    sha256 "8f7ee6b51defd977ae235b6aeeae487ce2582175cdcf22229497ba811642b31f"
-  elsif OS.windows?
+    sha256 "d16ca0eb9cfb630e6dba0b0e1b261b0f208a6598f7247b7a24e71ff2671bb8a0"
+  elsif OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/jeroenzeelmaekers/simls/releases/download/#{version}/simls-aarch64-apple-darwin.tar.gz"
+    sha256 "3eee73878a8d061851b292593d0454905b1937c70e719915c64a595737a7072f"
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/jeroenzeelmaekers/simls/releases/download/#{version}/simls-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "03d4f124de30515d9b64a2cd62adc2bd8bfc6d08e500028da93e7ad3c4de1e1b"
+  elsif OS.windows? && Hardware::CPU.intel?
     url "https://github.com/jeroenzeelmaekers/simls/releases/download/#{version}/simls-x86_64-pc-windows-msvc.zip"
-    sha256 "04ad00966a6a4ed99381958a3c65599311abf9f3258cdbd47af93dbdc91e298e"
-  else
-    odie "Unsupported operating system"
+    sha256 "93194d8bf6fb746648eb3da37590ca8d6314de3687b445c109c3e046a9c2b56c"
   end
   
   def install
-    unless OS.mac? || OS.windows?
-      odie "Unsupported operating system"
-    end
-  
-    if OS.mac?
+    if OS.mac? || OS.linux?
       bin.install "simls"
     elsif OS.windows?
       libexec.install Dir["*"]
